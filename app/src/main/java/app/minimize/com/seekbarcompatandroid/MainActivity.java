0f6935acaf4ecda0a4427e545f619a757da28b91
Ahmed Rizwan <ahmedrizwan@outlook.com>
2015-06-30 16:42:23 +0500

    Change colors in MainActivity

--- app/src/main/java/app/minimize/com/seekbarcompatandroid/MainActivity.java
+++ app/src/main/java/app/minimize/com/seekbarcompatandroid/MainActivity.java
@@ -1,9 +1,14 @@
 package app.minimize.com.seekbarcompatandroid;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import app.minimize.com.seek_bar_compat.SeekBarCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,20 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SeekBarCompat seekBarCompat = (SeekBarCompat) findViewById(R.id.materialSeekBar);
+        seekBarCompat.setThumbColor(Color.RED);
+        seekBarCompat.setProgressColor(Color.CYAN);
+
+        SeekBarCompat seekBarCompatTwo = (SeekBarCompat) findViewById(R.id.materialSeekBarTwo);
+        seekBarCompatTwo.setThumbColor(Color.BLUE);
+        seekBarCompatTwo.setProgressColor(Color.GREEN);
 
 
-//        SeekBarCompat seekBarCompatCode = (SeekBarCompat) View.inflate(this,R.layout.seekbar_compat, null);
-//        seekBarCompatCode.setThumbColor(Color.RED);
-//        ((LinearLayout) findViewById(R.id.container)).addView(seekBarCompatCode);
+        SeekBarCompat seekBarCompatCode = (SeekBarCompat) View.inflate(this, R.layout.seekbar_compat, null);
+        seekBarCompatCode.setThumbColor(Color.RED);
+        ((LinearLayout) findViewById(R.id.container)).addView(seekBarCompatCode);
 
-//        SeekBarCompat seekBarCompat = (SeekBarCompat) findViewById(R.id.materialSeekBar);
-//        seekBarCompat.setThumbColor(Color.RED);
-//        seekBarCompat.setProgressColor(Color.RED);
-//
-//
-//        SeekBarCompat seekBarCompatTwo = (SeekBarCompat) findViewById(R.id.materialSeekBarTwo);
-//        seekBarCompatTwo.setThumbColor(Color.BLUE);
-////        seekBarCompatTwo.setProgressColor(Color.RED);
 
     }
 
